5b04ab5a57e59f6a4f4ce568fd2d5baaac8ca40f
officeroco <68936526+officeroco@users.noreply.github.com>
2020-07-29 13:00:44 +0900

    Add files via upload

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            //全てのクライアントにメッセージを送信する。
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
